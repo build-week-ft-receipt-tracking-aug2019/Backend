8f751e19bf137e9be65d32a0f53c5fe14cb13a8d
onetrckchris <onetrckchris@gmail.com>
2019-08-26 10:14:29 -0700

    created basic server using express

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} request was made to ${req.url}`)
+    next();
+}
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(logger);
+
+module.exports = server;
